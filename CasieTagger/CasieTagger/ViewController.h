cd08a535eddf22c53df389080626dc2e11c1d81d
nadavGo <nadav.golan@orcam.com>
2015-11-26 15:21:03 +0200

    com

---
+++ CasieTagger/CasieTagger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CasieTagger
+//
+//  Created by orcam on 11/26/15.
+//  Copyright © 2015 orcam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
